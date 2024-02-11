156c0d5ed258a6f15fdfc564bf4e2d31612b3202
rainbowOutlaw <148523561+rainbowOutlaw@users.noreply.github.com>
2024-02-11 12:52:00 +0530

    feat: update ui according to response

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import Question from "./Question";
 
 const Body = () => {
+  const [solved, setSolved] = useState(0);
+
   return (
     <div className="flex flex-col items-center  gap-8 mt-7 font-poppins">
       <Question />
-      <div>1 of 5</div>
     </div>
   );
 };
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,26 +1,54 @@
+import { useState } from "react";
+import data from "../assets/data";
+
 const Question = () => {
+  const [index, setIndex] = useState(0);
+  const [question, setQuestion] = useState(data[index]);
+
+  const checkAnswer = (e, ans) => {
+    if (question.ans === ans) {
+      //TODO
+    } else {
+      //TODO
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 w-1/2">
       <h2 className="font-bold text-xl mb-3">
-        1. Which device is required for the Internet connection ?
+        {index + 1}. {question.question}
       </h2>
       <ul className="ml-2">
-        <li className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg">
-          Modem
+        <li
+          onClick={(e) => checkAnswer(e, 1)}
+          className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg hover:bg-blue-500"
+        >
+          {question.option1}
         </li>
-        <li className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg">
-          Router
+        <li
+          onClick={(e) => checkAnswer(e, 2)}
+          className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg hover:bg-blue-500"
+        >
+          {question.option2}
         </li>
-        <li className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg">
-          LAN cable
+        <li
+          onClick={(e) => checkAnswer(e, 3)}
+          className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg hover:bg-blue-500"
+        >
+          {question.option3}
         </li>
-        <li className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg">
-          Pen Drive
+        <li
+          onClick={(e) => checkAnswer(e, 4)}
+          className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg hover:bg-blue-500"
+        >
+          {question.option4}
         </li>
       </ul>
       <button className="p-2 mx-auto bg-slate-400 w-1/4 hover:bg-green-600 rounded-lg">
         Next
       </button>
+
+      <div className="mx-auto">{index + 1} of 5</div>
     </div>
   );
 };

2a648f16aa9c5bfd9afa9532b1cdf4867f34b30b
rainbowOutlaw <148523561+rainbowOutlaw@users.noreply.github.com>
2024-02-11 17:16:18 +0530

    feat: display results at the end of the quiz

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import Question from "./Question";
 
 const Body = () => {
-  const [solved, setSolved] = useState(0);
-
   return (
     <div className="flex flex-col items-center  gap-8 mt-7 font-poppins">
       <Question />
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,54 +1,111 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import data from "../assets/data";
 
 const Question = () => {
   const [index, setIndex] = useState(0);
   const [question, setQuestion] = useState(data[index]);
+  const [score, setScore] = useState(0);
+  const [lock, setLock] = useState(false);
+  const [result, setResult] = useState(false);
+
+  const optionOne = useRef(null);
+  const optionTwo = useRef(null);
+  const optionThree = useRef(null);
+  const optionFour = useRef(null);
+
+  const optionsArr = [optionOne, optionTwo, optionThree, optionFour];
 
   const checkAnswer = (e, ans) => {
-    if (question.ans === ans) {
-      //TODO
-    } else {
-      //TODO
+    if (!lock) {
+      if (question.ans === ans) {
+        //TODO
+        e.target.classList.add("bg-green-400");
+
+        setScore(score + 1);
+      } else {
+        //TODO
+        e.target.classList.add("bg-red-400");
+
+        optionsArr[question.ans - 1].current.classList.add("bg-green-400");
+      }
+
+      setLock(true);
+    }
+  };
+
+  const handleClick = () => {
+    if (lock) {
+      if (index === data.length - 1) {
+        setResult(true);
+        return;
+      }
+      setIndex(index + 1);
+      setQuestion(data[index + 1]);
+      setLock(false);
+      optionsArr.map((option) => {
+        option.current.classList.remove("bg-green-400");
+        option.current.classList.remove("bg-red-400");
+      });
     }
   };
 
   return (
     <div className="flex flex-col gap-4 w-1/2">
-      <h2 className="font-bold text-xl mb-3">
-        {index + 1}. {question.question}
-      </h2>
-      <ul className="ml-2">
-        <li
-          onClick={(e) => checkAnswer(e, 1)}
-          className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg hover:bg-blue-500"
-        >
-          {question.option1}
-        </li>
-        <li
-          onClick={(e) => checkAnswer(e, 2)}
-          className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg hover:bg-blue-500"
-        >
-          {question.option2}
-        </li>
-        <li
-          onClick={(e) => checkAnswer(e, 3)}
-          className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg hover:bg-blue-500"
-        >
-          {question.option3}
-        </li>
-        <li
-          onClick={(e) => checkAnswer(e, 4)}
-          className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg hover:bg-blue-500"
-        >
-          {question.option4}
-        </li>
-      </ul>
-      <button className="p-2 mx-auto bg-slate-400 w-1/4 hover:bg-green-600 rounded-lg">
-        Next
-      </button>
+      {result ? (
+        <></>
+      ) : (
+        <>
+          <h2 className="font-bold text-xl mb-3">
+            {index + 1}. {question.question}
+          </h2>
+          <ul className="ml-2">
+            <li
+              ref={optionOne}
+              onClick={(e) => checkAnswer(e, 1)}
+              className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg"
+            >
+              {question.option1}
+            </li>
+            <li
+              ref={optionTwo}
+              onClick={(e) => checkAnswer(e, 2)}
+              className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg"
+            >
+              {question.option2}
+            </li>
+            <li
+              ref={optionThree}
+              onClick={(e) => checkAnswer(e, 3)}
+              className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg"
+            >
+              {question.option3}
+            </li>
+            <li
+              ref={optionFour}
+              onClick={(e) => checkAnswer(e, 4)}
+              className="cursor-pointer border-[0.1rem] border-white mb-3 p-1 rounded-lg"
+            >
+              {question.option4}
+            </li>
+          </ul>
+          <button
+            onClick={handleClick}
+            className="p-2 mx-auto bg-slate-400 w-1/4 hover:bg-green-600 rounded-lg"
+          >
+            Next
+          </button>
 
-      <div className="mx-auto">{index + 1} of 5</div>
+          <div className="mx-auto">{index + 1} of 5</div>
+        </>
+      )}
+      {result && (
+        <div className="w-full text-center">
+          <h2>
+            You scored {score} out of {data.length}
+          </h2>
+          <button>Reset</button>
+        </div>
+      )}
     </div>
   );
 };
